420a5130d89669526582ae6c0b2b7cbaad674ffa
Dmitry Beliakov <hulk295@yandex.ru>
2023-03-09 21:50:58 +0300

    сделан вывод решения через comprehensive

--- Seminar05/task6-32.py
+++ Seminar05/task6-32.py
@@ -18,18 +18,7 @@ li=[] #автосборка массива
 for i in range(N): 
     li.append(random.randint(-10, 40))
 print(f'исходный массив:\n{li}')
-# li = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-ml = []
-mn = []
-mrange = [mi,ma]
-d = {}
-for i in range(len(li)):
-    if mrange[0]<li[i]<mrange[1]:
-        ml.append(i)
-        mn.append(li[i])
-        d[i]=li[i]
-
-print(f'в диапазон от {mrange[0]} до {mrange[1]} попадают элементы на позициях')
-print(*ml)
-print('для сверки («позиция»: «значение»)')
-print(d)
+print(f'в диапазон от {mi} до {ma} попадают элементы на позициях')
+print(*[i for i in range(len(li)) if mi<li[i]<ma])
+print('для сверки: («позиция», «значение»)')
+print(*[(i, li[i]) for i in range(len(li)) if mi<li[i]<ma])
